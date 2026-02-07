3a819bdc39dfd4ac85a04f82c21779cd7922d5f4
dedencreatifdeveloper <dedencreatifdeveloper@gmail.com>
2026-02-07 11:39:21 +0000

    datatable

--- resources/views/pages/pesanan/⚡pesanan-index.blade.php
+++ resources/views/pages/pesanan/⚡pesanan-index.blade.php
@@ -17,7 +17,7 @@ new #[Title('Pesanan')] class  extends Component {
 @push('css')
     <link rel="stylesheet" href="{{ asset('style/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}">
     <link rel="stylesheet" href="{{ asset('style/plugins/datatables-responsive/css/responsive.bootstrap4.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('/plugins/datatables-buttons/css/buttons.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('style/plugins/datatables-buttons/css/buttons.bootstrap4.min.css') }}">
 @endpush
 
 @push('js')
